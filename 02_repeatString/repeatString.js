60307c5c6aa534363a04c5e2c2c0db86b49ee051
Ezra <ezramalewis@gmail.com>
2022-05-10 16:34:45 -0400

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(repeat, n) {
+    let newString = "";
 
+    if (n < 0) {
+        return "ERROR";
+    }
+
+    for (let i = 0; i < n; i += 1) {
+        newString += repeat;
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
