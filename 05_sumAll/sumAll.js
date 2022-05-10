27466c1c14605e1de971176c990403fc9e63b7fc
Ezra <ezramalewis@gmail.com>
2022-05-10 17:47:59 -0400

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(...nums) {
+    if (nums.length > 2) {
+        return "ERROR";
+    }
+    if (nums[1] < 0 || nums[0] < 0) {
+        return "ERROR";
+    }
+    if (nums.some((elem) => {
+        return (typeof elem != "number");
+    })) {
+        return "ERROR";
+    };
 
+    nums.sort(function(a, b){return b - a});
+    let n = 0;
+
+    while (nums[0] >= nums[1]) {
+        n += nums[1];
+        nums[1] += 1;
+    }
+
+    return n;
 };
 
 // Do not edit below this line
