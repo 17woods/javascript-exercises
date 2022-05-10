21033af0bb0a129d3da6ad8021e407c81443adcd
Ezra <ezramalewis@gmail.com>
2022-05-10 16:43:55 -0400

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let newString = [];
+    for (let i in str) {
+        newString.unshift(str[i]);
+    }
+    newString = newString.join("");
+    return newString;
 };
 
 // Do not edit below this line
