75f68906f68c5ce5c15a68eded72361b0e7e4b8f
Ezra <ezramalewis@gmail.com>
2022-05-10 17:21:08 -0400

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...remove) {
+    let newArr = arr.filter((elem) => {
+        return !remove.includes(elem);
+    });
+    
+    return newArr;
 };
 
 // Do not edit below this line
